88cc6e4b76b872c3cfd7117136d37e31d1605d28
Abdullah <78679057+abdullah798155@users.noreply.github.com>
2025-11-19 22:16:49 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "abdullah-pwa-cache-v35";
+const CACHE_NAME = "abdullah-pwa-cache-v37";
 const ASSETS_TO_CACHE = [
   "/",
   "/index.html",
@@ -50,5 +50,6 @@ self.addEventListener("activate", (event) => {
     )
   );
 });
+
 
 
